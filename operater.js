bf87dc24b58a22d1e996f58bbb0adf4785864be2
Akramdadkhan <97161831+Akramdadkhan@users.noreply.github.com>
2022-02-25 15:25:42 +0530

    first commit

---
+++ operater.js
@@ -0,0 +1,25 @@
+// console.log(5 < 6 && 15 > 14 || 2>3 && 4>3)
+
+// console.log(5>6 || 6>7)
+
+// console.log(4!=="4")
+
+// console.log(5==5)
+
+// console.log(5 === "5")
+
+// console.log(3!=="3")
+
+// console.log(2 !==3)
+
+// console.log(5<=5)
+
+// console.log(-8<=6)
+
+// console.log(6>6)
+
+// console.log(6 >= 7)
+
+// console.log('2'+22)
+
+// document.write(22+2)
